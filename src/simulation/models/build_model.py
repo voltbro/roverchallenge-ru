72446f038b8d9d0cf696768ffd629c3a04e6cf5f
Igor Beschastnov <beschastnovigor@gmail.com>
2024-09-13 19:51:51 +0300

    field world and one wheel

--- src/simulation/models/build_model.py
+++ src/simulation/models/build_model.py
@@ -7,12 +7,16 @@ from xacro import process_file
 
 
 this_dir = Path(__file__).parent
-all_xacros = this_dir.rglob('*.*.xacro')
+all_xacros = this_dir.rglob("*.*.xacro")
 
 for xacro in all_xacros:
-    document: Document = process_file(xacro)  # type: ignore
+    try:
+        document: Document = process_file(xacro)  # type: ignore
+    except:
+        print(f"Error while processing {xacro}")
+        raise
     string = document.toprettyxml()
     target = xacro.parent / xacro.stem
     print(f"Processing: <{target}>")
-    with open(target, 'w') as target_file:
+    with open(target, "w") as target_file:
         target_file.write(string)
